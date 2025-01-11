9e661bea4fac65533beb46320c3839e6d1f9101e
MagicalInnkeep <TheMagicalInnkeeper@proton.me>
2025-01-11 19:40:49 +0100

    Solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeF) {
+  // °F ≘ (x − 32) × ⁠5/9⁠ °C 
+  let degreeC = (degreeF-32)*(5/9);
+  return Math.round(degreeC*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeC) {
+    // °C ≘ (x × ⁠9/5⁠ + 32) °F 	
+  let degreeF = degreeC *(9/5)+32;
+  return Math.round(degreeF*10)/10;
 };
 
 // Do not edit below this line
