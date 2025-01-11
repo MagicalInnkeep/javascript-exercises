12a85e9545e0f696f1ed4be23446855035d76de4
MagicalInnkeep <TheMagicalInnkeeper@proton.me>
2025-01-11 11:41:52 +0100

    Fails solution for 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,valueList) {;
+    for(var i=0;i<valueList.length;i++){
+        const index = array.getIndexOf(valueList[i]);
+        console.log(`Remove ${valueList[i]} from ${array}`)
+        if(index>=0){
+            array=array.splice(index,1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
