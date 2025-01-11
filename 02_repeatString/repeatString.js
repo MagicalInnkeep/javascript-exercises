2744c47e1d5e264a1088e9a7b47604e79320749b
MagicalInnkeep <TheMagicalInnkeeper@proton.me>
2025-01-11 10:31:24 +0100

    Solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    //function which repeats a given string a given number of times
+    let returnString="";
+    for(i=0;i<num;i++){
+        returnString+=string;
+    }
+    if(num<0){
+        returnString="ERROR";
+    }
+    return returnString
 };
 
 // Do not edit below this line
