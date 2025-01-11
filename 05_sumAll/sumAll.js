4e06d6bbbb8f26d3225f7c7bc689d05de9701dbc
MagicalInnkeep <TheMagicalInnkeeper@proton.me>
2025-01-11 19:23:25 +0100

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(x,y) {
+    //Create var to keep sum
+    let sum = 0
+    //Validate input and prevent non numeric values
+    if(!(Number.isInteger(x)) || !(Number.isInteger(y)) || x<0 || y<0){
+        return "ERROR";
+    }
+    //Then we need to define the lowest number
+    const minValue = x<=y ? x :y;
+    const maxValue = x>y ? x : y;
+    //Then loop while increasing the value.
+    for(i=minValue;i<=maxValue;i++){
+        sum=sum+i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
