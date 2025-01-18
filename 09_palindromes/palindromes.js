1e2acbcd8603b08e04fc1076cbfc427c17f93769
MagicalInnkeep <TheMagicalInnkeeper@proton.me>
2025-01-18 19:48:16 +0100

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,29 @@
-const palindromes = function () {
+function isAlphaNumeric(str) {
+    var code, i, len;
+  
+    for (i = 0, len = str.length; i < len; i++) {
+      code = str.charCodeAt(i);
+      if (!(code > 47 && code < 58) && // numeric (0-9)
+          !(code > 64 && code < 91) && // upper alpha (A-Z)
+          !(code > 96 && code < 123)) { // lower alpha (a-z)
+        return false;
+      }
+    }
+    return true;
+  };
 
+const palindromes = function (string) {
+    let reversed = string
+                    .split('')
+                    .filter(item => isAlphaNumeric(item))
+                    .reverse()
+                    .join('');
+    let normal = string
+                    .split('')
+                    .filter(item => isAlphaNumeric(item))
+                    .join('');
+    return normal.toUpperCase()==reversed.toUpperCase();         
+    
 };
 
 // Do not edit below this line
