f5b66990d9910205f269b24d0ea43fad6306171c
MagicalInnkeep <TheMagicalInnkeeper@proton.me>
2025-01-19 09:27:47 +0100

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const d = new Date();
+    const year = d.getFullYear();
 
+    for(let item in array){
+        if(!array[item].yearOfDeath){
+            array[item].yearOfDeath= year;
+        }
+    }
+    let arr2=array
+                .sort( (a,b) => 
+                (a.yearOfDeath-a.yearOfBirth) > (b.yearOfDeath-b.yearOfBirth) ? -1 : 1
+                )
+    return arr2[0];
+            
 };
 
 // Do not edit below this line
