23026679a6b8dd4da83ef7ef7fed7e5356143212
MagicalInnkeep <TheMagicalInnkeeper@proton.me>
2025-01-19 08:11:23 +0100

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(init) {
+    number= parseInt(init);
+    if(number==null){
+        return "OOPS";
+    }
+    else if (number<0){
+        return "OOPS";
+    }
+    else{
+    let fiboNo = 0;
+    let bufferNo =1;
+    let buffer2No =0;
+    for(let i=1; i<=number;i++){
+        fiboNo=bufferNo+buffer2No;
+        bufferNo=buffer2No;
+        buffer2No=fiboNo;
+    }
+    return fiboNo;
+    }
 };
 
 // Do not edit below this line
